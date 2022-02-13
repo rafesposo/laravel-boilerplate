6747a8f9cf2600e6234a214d430b4313cd47e5b8
Rafael Esposo <rafesposo@gmail.com>
2022-02-13 18:55:51 -0300

    Adicionado Default Layout - livewire

---
+++ resources/views/layouts/page.blade.php
@@ -0,0 +1,17 @@
+<nav>
+    <ul>
+        <li><a href="http://">Home</a></li>
+        <li><a href="http://">Produtos</a></li>
+        <li><a href="http://">Serviços</a></li>
+        <li><a href="http://">Contato</a></li>
+    </ul>
+</nav>
+
+
+<main>
+    {{ $slot }}
+</main>
+
+<footer>
+    <small>Todos os direitos reservados</small>
+</footer>
---
+++ resources/views/livewire/paginas/home.blade.php
@@ -0,0 +1,8 @@
+<x-page-layout>
+    <h1>Página inicial</h1>
+    <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis facilis animi eligendi aliquam voluptatum in
+        ullam doloribus hic sapiente repellendus. Fuga iusto cum tempora consequatur neque expedita, quam delectus
+        temporibus?
+    </p>
+</x-page-layout>
